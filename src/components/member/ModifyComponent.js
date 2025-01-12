49cc3685bbddb86be13c55364d0a406b126e6599
Yongmin222 <gkdlfn579@gmail.com>
2025-01-12 18:36:12 +0900

    feat: 마이페이지 로그아웃 추가 및 CSS 변경

--- src/components/member/ModifyComponent.js
+++ src/components/member/ModifyComponent.js
@@ -9,11 +9,12 @@ const initState = {
     email: "",
     password: "",
     nickname: "",
-    profileImage: "" // 프로필 이미지 추가
+    profileImage: "" 
 }
 
 const ModifyComponent = () => {
     const [member, setMember] = useState(initState);
+    const [confirmPassword, setConfirmPassword] = useState("");
     const loginInfo = useSelector(state => state.loginSlice);
     const { moveToPath } = useCustomLogin();
     const [result, setResult] = useState();
@@ -29,18 +30,28 @@ const ModifyComponent = () => {
     }, [loginInfo]);
 
     const handleChange = (e) => {
-        setMember({ ...member, [e.target.name]: e.target.value });
+        const { name, value } = e.target;
+        if (name === "confirmPassword") {
+            setConfirmPassword(value); // 비밀번호 확인 상태 업데이트
+        } else {
+            setMember({ ...member, [name]: value });
+        }
     }
 
     const handleClickModify = async () => {
-        try {
+            // 비밀번호와 비밀번호 확인 값이 같은지 확인
+            if (member.password !== confirmPassword) {
+                window.alert("비밀번호가 일치하지 않습니다.");
+                return; // 수정 진행 중지
+            }
+            try {
             const memberToUpdate = { ...member };
             if (!memberToUpdate.password) {
                 delete memberToUpdate.password;
             }
             await modifyMember(memberToUpdate, loginInfo.mno);
             dispatch(updateLoginInfo(memberToUpdate));
-            setResult('Modified');
+            setResult('수정완료');
         } catch (error) {
             setError(error.response ? error.response.data.message : error.message);
         }
@@ -67,49 +78,61 @@ const ModifyComponent = () => {
     return (
         <div className="mt-10 m-2 p-4"> {/* 테두리 관련 클래스 제거 */}
             {result ? <ResultModal title={'회원정보'} content={result} callbackFn={closeModal}></ResultModal> : null}
-            {error ? <div className="text-red-500">{error}</div> : null}
+            {error ? <div className="text-red-500 mb-4">{error}</div> : null}
 
-            <div className="flex justify-center">
-                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-                    <div className="p-6 text-right font-bold w-32">Email</div>
+        <div className="flex">
+        <div className="w-1/2 p-4">    
+            <div className="flex justify-center items-center h-full mb-4">
+                <div className="relative mb-4 flex w-full flex-wrap items-center">
                     <input className="p-2 border-b border-neutral-300 focus:outline-none focus:border-blue-500 flex-grow"
-                        name="email" type={'text'} value={member.email} readOnly />
+                        name="profileImage" type={'file'} value={member.profileImage} onChange={handleChange} />
+                    <div className="p-6 text-center font-bold w-full">프로필 이미지 수정</div>
+                    </div>
+                </div>
+            </div>            
+            <div className="w-1/2 p-4">
+                <div className="flex justify-center mb-4">
+                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                        <div className="p-6 text-right font-bold w-32">아이디</div>
+                        <input className="p-2 border-b border-neutral-300 focus:outline-none focus:border-blue-500 flex-grow"
+                            name="email" type={'text'} value={member.email} readOnly />
+                    </div>
+                </div>
+
+                <div className="flex justify-center">
+                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                        <div className="p-6 text-right font-bold w-32">본인 이름</div>
+                        <input className="p-2 border-b border-neutral-300 focus:outline-none focus:border-blue-500 flex-grow"
+                            name="nickname" type={'text'} value={member.nickname} onChange={handleChange} />
+                    </div>
+                </div>
+
+                <div className="flex justify-center">
+                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                        <div className="p-6 text-right font-bold w-32">비밀번호</div>
+                        <input className="p-2 border-b border-neutral-300 focus:outline-none focus:border-blue-500 flex-grow"
+                            name="password" type={'password'} value={member.password} onChange={handleChange} />
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                        <div className="p-6 text-right font-bold w-32">비밀번호 확인</div>
+                        <input className="p-2 border-b border-neutral-300 focus:outline-none focus:border-blue-500 flex-grow"
+                            name="confirmPassword" type={'password'} value={confirmPassword} onChange={handleChange} />
+                    </div>
                 </div>
             </div>
+        </div>    
 
             <div className="flex justify-center">
-                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-                    <div className="p-6 text-right font-bold w-32">Password</div>
-                    <input className="p-2 border-b border-neutral-300 focus:outline-none focus:border-blue-500 flex-grow"
-                        name="password" type={'password'} value={member.password} onChange={handleChange} />
-                </div>
-            </div>
-
-            <div className="flex justify-center">
-                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-                    <div className="p-6 text-right font-bold w-32">Nickname</div>
-                    <input className="p-2 border-b border-neutral-300 focus:outline-none focus:border-blue-500 flex-grow"
-                        name="nickname" type={'text'} value={member.nickname} onChange={handleChange} />
-                </div>
-            </div>
-
-            <div className="flex justify-center">
-                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-                    <div className="p-6 text-right font-bold w-32">Profile Image</div>
-                    <input className="p-2 border-b border-neutral-300 focus:outline-none focus:border-blue-500 flex-grow"
-                        name="profileImage" type={'text'} value={member.profileImage} onChange={handleChange} />
-                </div>
-            </div>
-
-            <div className="flex justify-center">
-                <div className="relative mb-4 flex w-full flex-wrap justify-end">
+                <div className="relative mb-4 flex w-full flex-wrap justify-center">
                     <button type="button" onClick={handleClickRemove}
                         className="rounded p-4 m-2 text-xl w-32 text-white bg-red-500">
-                        Remove
+                        탈퇴하기
                     </button>
                     <button type="button" onClick={handleClickModify}
                         className="rounded p-4 m-2 text-xl w-32 text-white bg-neutral-500">
-                        Modify
+                        수정하기
                     </button>
                 </div>
             </div>
--- src/components/member/MyPageComponent.js
+++ src/components/member/MyPageComponent.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux'; // useSelector import 추가
 import { Link } from 'react-router-dom';
+import useCustomLogin from "../../hooks/useCustomLogin";
+
 
 
 const MyPageComponent = () => {
+
     // Redux에서 로그인 상태 가져오기
     const loginState = useSelector(state => state.loginSlice);
 
@@ -23,38 +26,52 @@ const MyPageComponent = () => {
         });
     }, [loginState]);
 
+        const { doLogout, moveToPath } = useCustomLogin(); // useCustomLogin 훅 호출
+            const handleLogout = useCallback(() => {
+            doLogout();
+            alert("정상적으로 로그아웃되었습니다!");
+            moveToPath("/");
+        }, [doLogout, moveToPath]);
+        
     return (
-        <div style={{fontSize: '25px'}} >
-            <div style={{ padding: '20px', position: 'absolute', top: '100px', left: '0' }}>
-                <img 
-                    src={userInfo.profileImage}
-                    alt="프로필 사진"
-                    style={{ width: '50px', height: '50px', borderRadius: '50%', marginRight: '10px' }} 
-                />
+        <>
+            <div style={{fontSize: '25px', display:'flex', flexDirection:'row', width:'100%', height:'100%', alignItems:'center', justifyContent:'center'}} >
+                <div style={{ position: 'fixed', top: '65px', left: '10px'}}>
+                <button className="bg-gray-400 text-white px-4 py-2 rounded-lg hover:bg-gray-500 w-24 h-8 flex items-center justify-center shadow-md hover:shadow-lg transition-shadow text-sm"
+                                onClick={handleLogout}
+                            >로그아웃</button>
+                    <img 
+                        src={userInfo.profileImage}
+                        alt="프로필 사진"
+                        style={{ width: '150px', height: '70px', borderRadius: '50%', marginRight: '10px', marginTop: '10px'}} 
+                    />
+                    <div>
+                        <h3>닉네임: {userInfo.nickname}</h3>
+                        <p>이메일: {userInfo.email}</p>
+                    </div>
+                </div>
                 <div>
-                    <h3>닉네임: {userInfo.nickname}</h3>
-                    <p>이메일: {userInfo.email}</p>
+                    <div style={{ textAlign: 'center'}}>
+                    <h2><strong>프로필</strong></h2>
+                    <Link to="/member/modify">
+                        <button>프로필 수정</button>
+                    </Link>
+                    <h2 style={{marginTop:'20px'}}><strong>게시글</strong></h2>
+                    <Link to={'/board/lost'}>
+                        <button>내 분실물 게시글 조회</button>
+                    </Link><br/> 
+                    <Link to={'/board/found'}>
+                        <button>내 습득물 게시글 조회</button>
+                    </Link><br/> 
+                    <Link to={'/board/free'}>
+                        <button>내 자유 게시글 조회</button>
+                    </Link><br/> 
+                    </div>
                 </div>
             </div>
-            <div style={{ textAlign: 'center'}}>
-            <h2><strong>프로필</strong></h2>
-            <Link to="/member/modify">
-                <button>프로필 수정</button>
-            </Link>
-
-            <h2 style={{marginTop:'20px'}}><strong>게시글</strong></h2>
-            <Link to={'/board/lost'}>
-                <button>내 분실물 게시글 조회</button>
-            </Link><br/> 
-            <Link to={'/board/found'}>
-                <button>내 습득물 게시글 조회</button>
-            </Link><br/> 
-            <Link to={'/board/free'}>
-                <button>내 자유 게시글 조회</button>
-            </Link><br/> 
-            </div>
-        </div>
+        </>
     );
 };
+
 
 export default MyPageComponent;
